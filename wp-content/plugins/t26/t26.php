b5afcd4abed67d2d2e3a8e54249bf355772a79b7
AlexG <alex@alexgoldberg.online>
2023-12-01 17:28:35 +0200

    add plugin, add plugin headers

---
+++ wp-content/plugins/t26/t26.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Тестовое № 26
+ * Description: Тестовое №26. Плагин конвертации валют
+ * Plugin URI:  https:\\latul.website
+ * Author URI:  https:\\latul.website
+ * Author:      Webazex
+ *
+ * Text Domain: t26
+ *
+ * Requires PHP: 7.1
+ * Requires at least: 6.1
+ *
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Network:     false
+ * Version:     1.0
+ */
+
+// код плагина
